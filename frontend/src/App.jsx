0ba5427587c50e6fc51c09e63ebac4e2f4740f2e
samwi <samwinstown@gmail.com>
2025-10-18 11:47:46 +0300

    1) Сделал рендер полей отправки данных и кнопку. 2) Данные отправляются на сервер 3) Настроил прокси в vite.config, потому что запросы отправлялись не на порт бэкенда а на самого себя.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,12 @@
-    return (
-      </div>
-}
+import Form from './components/Form/Form.jsx'
+import './css/app.css'
 
+export default function App() {
+      return (
+          <div className="wrapper">
+            <main className='content-box'>
+                  <Form />
+            </main>
+          </div>
+      )
+}
